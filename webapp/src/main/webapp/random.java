8c479134ba76b944ec5b5f6046150b48a67b5bf9
Canterla11 <acanterlagonzalez@gmail.com>
2025-01-28 14:21:10 +0100

    Update random.java

--- webapp/src/main/webapp/random.java
+++ webapp/src/main/webapp/random.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/random")
+@WebServlet("/")
 public class RandomNumberServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
